d58c2b71a570b2efb96863e4f343a0a597afcd04
albertoBarl <alberto.barletta00@gmail.com>
2023-01-11 17:02:48 +0100

    begin header, added logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,17 @@
 <script>
 export default {
   name: "AppHeader",
+  data() {
+    return {
+      logo: "../src/assets/img/dc-logo.png",
+    };
+  },
 };
 </script>
-<template lang="">prova</template>
+<template lang="">
+  <header>
+    <div class="logo"><img :src="logo" alt="not available" /></div>
+    <nav></nav>
+  </header>
+</template>
 <style lang="sass"></style>
