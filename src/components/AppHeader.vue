124be70b8609b0e6ebc267bd9facb65856294d76
albertoBarl <alberto.barletta00@gmail.com>
2023-01-11 17:16:47 +0100

    added nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,49 @@ export default {
   name: "AppHeader",
   data() {
     return {
-      logo: "../src/assets/img/dc-logo.png",
+      logo: "src/assets/img/dc-logo.png",
+      nav: [
+        {
+          label: "Characters",
+          status: "not-active",
+        },
+        {
+          label: "Comics",
+          status: "not-active",
+        },
+        {
+          label: "Movies",
+          status: "not-active",
+        },
+        {
+          label: "Tv",
+          status: "not-active",
+        },
+        {
+          label: "Games",
+          status: "not-active",
+        },
+        {
+          label: "Collectibles",
+          status: "not-active",
+        },
+        {
+          label: "Videos",
+          status: "not-active",
+        },
+        {
+          label: "Fans",
+          status: "not-active",
+        },
+        {
+          label: "News",
+          status: "not-active",
+        },
+        {
+          label: "Shop",
+          status: "not-active",
+        },
+      ],
     };
   },
 };
@@ -11,7 +53,13 @@ export default {
 <template lang="">
   <header>
     <div class="logo"><img :src="logo" alt="not available" /></div>
-    <nav></nav>
+    <nav>
+      <ul>
+        <li v-for="(element, index) in nav">
+          <a href="/">{{ nav[index].label }}</a>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 <style lang="sass"></style>
