632705afa8a1a28e3ae259a4e95ae0bda2fc8b39
albertoBarl <alberto.barletta00@gmail.com>
2023-01-12 17:57:08 +0100

    created main, added props

---
+++ src/components/AppMain.vue
@@ -0,0 +1,32 @@
+<script>
+import AppCard from "../components/elements/AppCard.vue";
+import Cards from "../assets/datas/dataSeries.js";
+
+export default {
+  name: "AppMain",
+  components: {
+    AppCard,
+    Cards,
+  },
+  data() {
+    return {
+      seriesCard: Cards,
+    };
+  },
+};
+</script>
+<template lang="">
+  <div class="bg-dark">
+    <div class="container d-flex flex-wrap gap-5 p-5">
+      <AppCard
+        v-for="(elem, index) in seriesCard"
+        :key="index"
+        :dcSeries="elem"
+      />
+    </div>
+  </div>
+</template>
+<style lang="scss">
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+</style>
---
+++ src/components/elements/AppCard.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  props: { dcSeries: Object },
+};
+</script>
+<template lang="">
+  <div class="card">
+    <img :src="dcSeries.thumb" alt="not available" />
+    <span>{{ dcSeries.series }}</span>
+  </div>
+</template>
+<style lang="scss">
+div.card {
+  width: 150px;
+  height: auto;
+}
+</style>
